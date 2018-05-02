5dcb95d1cd631a0533c27e8068f2815d91d27bfc
Peter Ndirpaya <petes4christ@gmail.com>
2018-05-02 12:54:57 +0100

    add uikit to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="uk-margin-large-top">
+        <header className="uk-margin">
+          <h1 className="uk-heading-primary uk-align-bottom ">Welcome to React</h1>
+          <div className="uk-child-width-1-3@m uk-grid-small uk-grid-match" data-uk-grid>
+    <div>
+        <div className="uk-card uk-card-default uk-card-body">
+            <h3 className="uk-card-title">Default</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+    </div>
+    <div>
+        <div className="uk-card uk-card-primary uk-card-body">
+            <h3 className="uk-card-title">Primary</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+    </div>
+    <div>
+        <div className="uk-card uk-card-secondary uk-card-body">
+            <h3 className="uk-card-title">Secondary</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+    </div>
+</div>
         </header>
-        <p className="App-intro">
+        <p className="">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import UIkit from 'uikit';
+import style from 'uikit/dist/css/uikit.css'
+import Icons from 'uikit/dist/js/uikit-icons';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+UIkit.use(Icons);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
