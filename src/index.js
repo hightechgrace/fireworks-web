0ddafa733aaff17bd453e428807920e8d003a9a4
Peter Ndirpaya <petes4christ@gmail.com>
2018-05-03 12:05:13 +0100

    add react router dom

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ import style from 'uikit/dist/css/uikit.css'
 import Icons from 'uikit/dist/js/uikit-icons';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 UIkit.use(Icons);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const fireworks = () =>{
+<Router>
+    <div>
+        <Switch>
+        </Switch>
+    </div>
+</Router>
+
+    
+}
+
+ReactDOM.render(<fireworks />, document.getElementById('root'));
 registerServiceWorker();
