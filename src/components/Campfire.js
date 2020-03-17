711a6f9fa330e8fcd452401159afdb0bb19afcc1
Peter Ndirpaya <ndirpaya@peters-macbook-pro.local>
2020-03-17 11:53:02 +0100

    add campfire link for march 2020

--- src/components/Campfire.js
+++ src/components/Campfire.js
@@ -16,9 +16,9 @@ class Contact extends Component {
 
 
                             <h1 className='uk-text-left uk-heading-primary'>An Interactive Gathering of <br />Creatives & Entrepreneurs </h1><br />
-                            <p>Fri, February 21, 2020,  5:00 PM – 7:00 PM </p>
+                            <p>Fri, March 20, 2020,  5:00 PM – 7:00 PM </p>
                             <br />
-                            <a href='https://www.eventbrite.com/e/campfire-an-interactive-gathering-of-creative-entrepreneurs-tickets-95366938079 <https://www.eventbrite.com/e/campfire-an-interactive-gathering-of-creative-entrepreneurs-tickets-95366938079?aff=utm_source%3Deb_email%26utm_medium%3Demail%26utm_campaign%3Dnew_event_email&utm_term=eventurl_text>' target='blank_' className="uk-button uk-button-secondary">Register</a>
+                            <a href='https://www.eventbrite.com/e/campfire-an-interactive-gathering-of-creative-entrepreneurs-march-2020-tickets-99973467332' target='blank_' className="uk-button uk-button-secondary">Register</a>
                         </div>
                     </div>
                 </div>
